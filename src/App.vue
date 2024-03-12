ae51a489294b1a8d5fefe1bab3d1831c83445b29
newGustavo <gustavonascimento1709@gmail.com>
2024-03-12 10:09:15 -0500

    altera a base do sistema

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <script setup>
 import IconPrefeitura from './components/icons/IconPrefeitura.vue'
 import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
+import IconEngrenagem from './components/icons/IconEngrenagem.vue';
 </script>
 
 <template>
-  <header class="flex justify-center">
-    <IconPrefeitura />
-  </header>
+  <!--
+    This example requires updating your template:
 
-  <main class="py-4 flex justify-center">
-    <ConteudoPrincipal />
+    ```
+    <html class="h-full">
+    <body class="h-full">
+    ```
+  -->
+  <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+    <div class="text-center">
+      <p class="font-semibold text-indigo-600 text-2xl">503</p>
+      <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Sistema em manutenção</h1>
+      <p class="mt-6 text-base leading-7 text-gray-600">Tente novamente mais tarde.</p>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+        <IconEngrenagem />
+      </div>
+    </div>
+    <IconPrefeitura class="mt-8" />
+    <div class="flex flex-row justify-center">
+    </div>
   </main>
-
 </template>
 
 <style scoped>
-
 </style>
--- src/components/ConteudoPrincipal.vue
+++ src/components/ConteudoPrincipal.vue
@@ -1,11 +1,26 @@
+<script setup>
+
+import IconEngrenagem from '../components/icons/IconEngrenagem.vue'
+
+
+</script>
+
 <template>
-    <div class="pt-4">
-        <h1 class="text-4xl font-bold box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 font-white">
-            Sistema em manutenção!
-        </h1>
-    </div>
-</template>
+    <!--
+      This example requires updating your template:
+  
+      ```
+      <html class="h-full">
+      <body class="h-full">
+      ```
+    -->
+    <main class="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div class="text-center">
+        <p class="text-base font-semibold text-indigo-600">404</p>
+        <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Sistema em manutenção!</h1>
+        <p class="mt-6 text-base leading-7 text-gray-600">Tente Novamente mais tarde!</p>
+      </div>
+    </main>
+  </template>
 
-<style scoped>
 
-</style>
