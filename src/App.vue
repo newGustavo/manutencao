1b69f87c9ca9abc3f43a3e2444f0ccbbe586a14a
newGustavo <gustavonascimento1709@gmail.com>
2024-03-11 19:02:24 -0500

    primeiros arquivos

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import IconPrefeitura from './components/icons/IconPrefeitura.vue'
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue'
+</script>
+
+<template>
+  <header class="flex justify-center">
+    <IconPrefeitura />
+  </header>
+
+  <main class="py-4 flex justify-center">
+    <ConteudoPrincipal />
+  </main>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="pt-4">
+        <h1 class="text-4xl font-bold box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 font-white">
+            Sistema em manutenção!
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
